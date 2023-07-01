8c99cbb94b49c09b8b68531c36905d876e7be13c
Loris Caputo <lorixyes@gmail.com>
2023-07-02 00:37:38 +0200

    Changes

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const mysql = require('mysql');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 // Configurazione del database
 const dbConfig = {
