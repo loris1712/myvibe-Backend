2e2522cc767a01c687b36bebe644561e615edcfe
Loris Caputo <lorixyes@gmail.com>
2023-08-26 17:32:40 +0200

    Changes

--- routes/places.js
+++ routes/places.js
@@ -82,7 +82,7 @@ router.get('/filteredHomePlaces', (req, res) => {
   const queryParams = [cityName];
 
   if (req.query.filter) {
-    whereClause += " AND ns.category LIKE ?";
+    whereClause += " AND ns.category = ?";
     queryParams.push(filter);
   }
 
