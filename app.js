1a814b395a2b4fc7f14ed426d05a582d2e5684df
Loris Caputo <lorixyes@gmail.com>
2023-10-10 21:33:16 +0200

    Changes

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors'); // Aggiungi questa riga per importare il modulo CORS
+
 app.use(bodyParser.json());
+app.use(cors()); // Usa il middleware CORS per abilitare le richieste da tutti gli origini
 
 const placesRoutes = require('./routes/places');
 const usersRoutes = require('./routes/users');
